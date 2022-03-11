27e7d058ca7bdcd2f30046f4c2d6a78ff3f81055
vKxni <Konradtoesch@icloud.com>
2022-03-11 18:11:42 +0100

    types

--- lib/counter/application.ex
+++ lib/counter/application.ex
@@ -29,6 +29,7 @@ defmodule Counter.Application do
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   @impl true
+  @spec config_change(any, any, any) :: :ok
   def config_change(changed, _new, removed) do
     CounterWeb.Endpoint.config_change(changed, removed)
     :ok
